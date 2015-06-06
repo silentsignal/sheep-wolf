c0b1666e030b01a2183e960f6ebbd51ac001bcf4
B <buherator@silentsignal.hu>
2015-06-06 15:00:50 +0200

    Removed obsolete code

--- evilize/evilize.c
+++ evilize/evilize.c
@@ -32,81 +32,6 @@ static char *crib = CRIB;
 #define LBUFSIZE 1024
 #define FASTCOLL_CMD "..\\\\fastcoll\\\\fastcoll.exe -i %08x%08x%08x%08x -o a b"
 
-/* write special purpose format file, compatible with md5coll. */
-static void write_collision_file(FILE *fout, unsigned int m0[32], unsigned int m1[32]) {
-  int i;
-  
-  fprintf(fout, "unsigned int m0[32] = {\n");
-  for (i=0; i<32; i++) {
-      fprintf(fout, "0x%08x, ", m0[i]);
-      if ((i & 3) == 3) {
-	fprintf(fout, "\n");
-      }
-  }
-  fprintf(fout, "};\n\n");
-  fprintf(fout, "unsigned int m1[32] = {\n");
-  for (i=0; i<32; i++) {
-    fprintf(fout, "0x%08x, ", m1[i]);
-    if ((i & 3) == 3) {
-      fprintf(fout, "\n");
-    }
-  }
-  fprintf(fout, "};\n\n");
-  return;
-}
-
-/* read special purpose format file, compatible with md5coll. Return
-   number of items successfully read (should be 64 on success). */
-static int read_collision_file(FILE *fin, unsigned int m0[32], unsigned int m1[32]) {
-  char buf[100];
-  int i = 0;
-  int j = 0;
-  unsigned int *m = m0;
-  char *r;
-  int c;
-
-  while (j<64) {
-    r = fgets(buf, 100, fin);
-    if (r == NULL) {
-      break;
-    }
-    c = sscanf(buf, "0x%x, 0x%x, 0x%x, 0x%x,", &m[i], &m[i+1], &m[i+2], &m[i+3]);
-    if (c==4) {
-      i+=4;
-      j+=4;
-      if (i==32) {
-	i = 0;
-	m = m1;
-      }
-    }
-  }
-  return j;
-}
-
-/* return allocated string, or NULL with errno set on error. */
-static char *append_string(char *s1, char *s2) {
-  char *s;
-
-  s = (char *)malloc(strlen(s1)+strlen(s2)+1);
-  if (!s) {
-    return NULL;
-  }
-  strcpy(s, s1);
-  strcat(s, s2);
-  return s;
-}
-
-static void paste_swap(unsigned int *m, char *c) {
-  int i;
-
-  for (i=0; i<32; i++) {
-    *c++ = m[i] & 0xff;
-    *c++ = (m[i] >> 8) & 0xff;
-    *c++ = (m[i] >> 16) & 0xff;
-    *c++ = (m[i] >> 24) & 0xff;
-  }
-}
-
 /* find the initial vector required to evilize FILE, return it in
    IV. Returns 0 on success, 1 if crib was not found. */
 static int find_iv(FILE *fin, unsigned int IV[4]) {
@@ -139,79 +64,6 @@ static int find_iv(FILE *fin, unsigned int IV[4]) {
   return ftell(fin);
 }
 
-/* evilize the given file fin as fout, using the collision blocks m0
-   and m1 previously found by md5coll. Replace first crib string using
-   m0, and second one using m1. Return 0 on success, 1 if first crib
-   not found, 2 if second crib not found. */
-static int evilize(FILE *fin, FILE *fout, unsigned int m0[32], unsigned int m1[32]) {
-  char lbuf[LBUFSIZE];
-  int r;  /* number of bytes in lbuf */
-  int i, j;
-
-  r = 0;
-
-  while (1) {
-    r += fread(lbuf+r, 1, LBUFSIZE-r, fin); /* fill buffer */
-    if (r<128) {
-      return 1;
-    }
-    for (j=0; j<r-127; j+=64) {
-      for (i=0; i<64; i++) {
-	if (memcmp(lbuf+j, crib+i, 128) == 0) {
-	  goto crib_found;
-	}
-      }
-    }
-    fwrite(lbuf, 1, j, fout);  /* empty j characters from buffer */
-    memmove(lbuf, lbuf+j, r-j);
-    r -= j;
-  }
- crib_found:
-  paste_swap(m0, lbuf+j);
-  j += 128;
-  fwrite(lbuf, 1, j, fout);  /* empty j characters from buffer */
-  memmove(lbuf, lbuf+j, r-j);
-  r -= j;
-
-  /* Now replace the next occurence of crib+i[0..63] by m1 */
-  while (1) {
-    r += fread(lbuf+r, 1, LBUFSIZE-r, fin); /* fill buffer */
-    if (r<128) {
-      return 1;
-    }
-    for (j=0; j<r-127; j++) {
-      if (memcmp(lbuf+j, crib+i, 128) == 0) {
-	goto crib2_found;
-      }
-    }
-    fwrite(lbuf, 1, j, fout);  /* empty non-crib part of buffer */
-    memmove(lbuf, lbuf+j, r-j);
-    r -= j;
-  }
- crib2_found:
-  paste_swap(m1, lbuf+j);
-
-  /* continue emptying buffer */
-  do {
-    fwrite(lbuf, 1, r, fout);
-    r = fread(lbuf, 1, LBUFSIZE, fin);
-  } while (r>0);
-
-  return 0;
-}
-
-#define COL0 "\033[G"
-#define CTLK "\033[K"
-
-/* a callback funciton to print progress information */
-static int progress_printer_f(void *priv, char *arg) {
-  fprintf(stdout, ""COL0"Progress: %s"CTLK"", arg);
-  fflush(stdout);
-  return 0;
-}
-
-static callback_t progress_printer = { &progress_printer_f, NULL};
-
 /* print usage information */
 
 static void usage(FILE *fout) {
@@ -224,11 +76,6 @@ static void usage(FILE *fout) {
 "    -h, --help              print this help message and exit\n"
 "    -V, --version           print version info and exit\n"
 "    -L, --license           print license info and exit\n"
-"    -g, --goodfile <file>   file to write 'good' output to\n"
-"    -e, --evilfile <file>   file to write 'evil' output to\n"
-"    -i, --iv                create initial vectors only\n"
-"    -c, --collision <file>  use pre-computed md5 collision blocks from file\n"
-"    -s, --seed <n>          specify random seed\n"
 "    --                      end of options, filename follows\n");
 }
 
@@ -270,14 +117,11 @@ static struct option longopts[] = {
   {"help",         0, 0, 'h'},
   {"version",      0, 0, 'V'},
   {"license",      0, 0, 'L'},
-  {"goodfile",     1, 0, 'g'},
-  {"evilfile",     1, 0, 'e'},
   {"iv",           0, 0, 'i'},
-  {"seed",         1, 0, 's'},
   {0, 0, 0, 0}
 };
 
-static char *shortopts = "hVLg:e:ic:s:";
+static char *shortopts = "hVLi";
 
 uint32_t swap_uint32( uint32_t val )
 {
@@ -288,12 +132,9 @@ uint32_t swap_uint32( uint32_t val )
 int main(int ac, char *av[]) {
   int r;
   unsigned int IV[4];
-  unsigned int m0[32];
-  unsigned int m1[32];
   int c;
   int count;
   FILE *fin;
-  FILE *fout;
   FILE *fa;
   FILE *fb;
   FILE *fsheep;
@@ -305,15 +146,9 @@ int main(int ac, char *av[]) {
   unsigned char copybuf[192];
   unsigned char ch;
   unsigned char ch_wolf,ch_sheep;
-  callback_t *cb = &progress_printer;
-  unsigned int seed = time(NULL) ^ (getpid() << 16);
   char *cmd;
 
   /* command line options */
-  int iv_only = 0;
-  char *collisionfile = NULL;
-  char *goodfile = NULL;
-  char *evilfile = NULL;
   char *infile = NULL;
 
   while ((c = getopt_long(ac, av, shortopts, longopts, NULL)) != -1) {
@@ -330,21 +165,6 @@ int main(int ac, char *av[]) {
       license(stdout);
       exit(0);
       break;
-    case 'i':
-      iv_only = 1;
-      break;
-    case 'c':
-      collisionfile = optarg;
-      break;
-    case 'g':
-      goodfile = optarg;
-      break;
-    case 'e':
-      evilfile = optarg;
-      break;
-    case 's':
-      seed = strtoul(optarg, NULL, 0);
-      break;
     case '?':
       fprintf(stderr, "Try --help for more information.\n");
       exit(1);
@@ -360,140 +180,63 @@ int main(int ac, char *av[]) {
     exit(1);
   }
   infile = av[optind];
-
-  if (!goodfile && !evilfile) {
-    goodfile = append_string(infile, ".good");
-    evilfile = append_string(infile, ".evil");
+    
+  fin = fopen(infile, "rb");
+  if (!fin) {
+    fprintf(stderr, "%s: %s: %s\n", NAME, infile, strerror(errno));
+    exit(1);
   }
-
-  if (!collisionfile) {
     
-    fin = fopen(infile, "rb");
-    if (!fin) {
-      fprintf(stderr, "%s: %s: %s\n", NAME, infile, strerror(errno));
-      exit(1);
-    }
-    
-    r = find_iv(fin, IV);
-    fclose(fin);
+  r = find_iv(fin, IV);
+  fclose(fin);
 	
-    if (r == 1) {
-      fprintf(stderr, "%s: %s: no crib found.\n", NAME, infile);
-      exit(1);
-    }
-    pos=r-strlen(CRIB);
-	printf("Pos: %d\n",pos);
-    fprintf(stdout, "%08x%08x%08x%08x\n", swap_uint32(IV[0]), swap_uint32(IV[1]), swap_uint32(IV[2]), swap_uint32(IV[3]));
-    cmd=(char*)malloc(strlen(FASTCOLL_CMD)+16+1);
-	sprintf(cmd,FASTCOLL_CMD,swap_uint32(IV[0]), swap_uint32(IV[1]), swap_uint32(IV[2]), swap_uint32(IV[3]));
-	printf("%s\n",cmd);
-    if (iv_only) {
-	  system(cmd);
-	  //printf("Run %s and press any key to continue",cmd);
-	  //getchar();
-	  fa=fopen("a","rb");
-	  fb=fopen("b","rb");
-	  fread(a,128,1,fa);
-	  fread(b,128,1,fb);
-	  fclose(fa);
-	  fclose(fb);
-	  fin = fopen(infile, "rb");
-	  fsheep = fopen("sheep.exe","wb"); 
-	  fwolf = fopen("wolf.exe","wb");
-	  while(!feof(fin)){
-		ch=fgetc(fin);
-		
-		if (ftell(fin)>pos && ftell(fin)<=pos+128){
-			//fputc(a[ftell(fin)-pos-1],fsheep);
-			//fputc(b[ftell(fin)-pos-1],fwolf);
-			ch_sheep=a[ftell(fin)-pos-1];
-			ch_wolf=b[ftell(fin)-pos-1];
-		}
-		else{
-			ch_wolf=ch;
-			ch_sheep=ch;
-		}
-		if (ftell(fin)>pos-16 && ftell(fin)<=pos+176){
-			copybuf[i++]=ch_wolf;
-		}
-		if (ftell(fin)>pos+176 && ch>0 && i>0){
-			ch_wolf=copybuf[192-i];
-			ch_sheep=copybuf[192-i];
-			i--;
-		}
-		fputc(ch_sheep,fsheep);
-		fputc(ch_wolf,fwolf);
-	  }
-	  fclose(fwolf);
-	  fclose(fsheep);
-	  fclose(fin);
-      return 0;
-    }
-    /*
-    fprintf(stdout, "Searching for MD5 collision (this can take several hours)...\n");
-    fprintf(stdout, "Random seed: %u\n", seed);
-    srandom(seed);
-    md5coll_with_iv(IV, m0, m1, cb);
-    fprintf(stdout, "\n");
-    fprintf(stdout, "Collision blocks:\n");
-    write_collision_file(stdout, m0, m1);
-    */
-  } else {
-    
-    fin = fopen(collisionfile, "r");
-    if (!fin) {
-      fprintf(stderr, "%s: %s: %s\n", NAME, collisionfile, strerror(errno));
-      exit(1);
-    }
-    r = read_collision_file(fin, m0, m1);
-    fclose(fin);
-    if (r != 64) {
-      fprintf(stderr, "%s: %s: bad file format, %d of 64 values read\n", NAME, collisionfile, r);
-      exit(1);
-    }
+  if (r == 1) {
+    fprintf(stderr, "%s: %s: no crib found.\n", NAME, infile);
+    exit(1);
   }
+  pos=r-strlen(CRIB);
+  printf("Pos: %d\n",pos);
+  fprintf(stdout, "%08x%08x%08x%08x\n", swap_uint32(IV[0]), swap_uint32(IV[1]), swap_uint32(IV[2]), swap_uint32(IV[3]));
+  cmd=(char*)malloc(strlen(FASTCOLL_CMD)+16+1);
+  sprintf(cmd,FASTCOLL_CMD,swap_uint32(IV[0]), swap_uint32(IV[1]), swap_uint32(IV[2]), swap_uint32(IV[3]));
+  printf("%s\n",cmd);
 
-  if (goodfile) {
-    fin = fopen(infile, "rb");
-    if (!fin) {
-      fprintf(stderr, "%s: %s: %s\n", NAME, infile, strerror(errno));
-      exit(1);
-    }
-    fout = fopen(goodfile, "wb");
-    if (!fout) {
-      fprintf(stderr, "%s: %s: %s\n", NAME, goodfile, strerror(errno));
-    } else {
-      fprintf(stdout, "Writing 'good' file %s.\n", goodfile);
-      r = evilize(fin, fout, m1, m1);
-      if (r) {
-	fprintf(stderr, "%s: %s: not a valid template file, crib not found\n", NAME, infile);
-	exit(1);
-      }
-      fclose(fout);
-    }
-    fclose(fin);
+  system(cmd);
+
+  fa=fopen("a","rb");
+  fb=fopen("b","rb");
+  fread(a,128,1,fa);
+  fread(b,128,1,fb);
+  fclose(fa);
+  fclose(fb);
+  fin = fopen(infile, "rb");
+  fsheep = fopen("sheep.exe","wb"); 
+  fwolf = fopen("wolf.exe","wb");
+  while(!feof(fin)){
+	ch=fgetc(fin);
+		
+	if (ftell(fin)>pos && ftell(fin)<=pos+128){
+		ch_sheep=a[ftell(fin)-pos-1];
+		ch_wolf=b[ftell(fin)-pos-1];
+	}
+	else{
+		ch_wolf=ch;
+		ch_sheep=ch;
+	}
+	if (ftell(fin)>pos-16 && ftell(fin)<=pos+176){
+		copybuf[i++]=ch_wolf;
+	}
+	if (ftell(fin)>pos+176 && ch>0 && i>0){
+		ch_wolf=copybuf[192-i];
+		ch_sheep=copybuf[192-i];
+		i--;
+	}
+	fputc(ch_sheep,fsheep);
+	fputc(ch_wolf,fwolf);
   }
-  
-  if (evilfile) {
-    fin = fopen(infile, "rb");
-    if (!fin) {
-      fprintf(stderr, "%s: %s: %s\n", NAME, infile, strerror(errno));
-      exit(1);
-    }
-    fout = fopen(evilfile, "wb");
-    if (!fout) {
-      fprintf(stderr, "%s: %s: %s\n", NAME, evilfile, strerror(errno));
-    } else {
-      fprintf(stdout, "Writing 'evil' file %s.\n", evilfile);
-      r = evilize(fin, fout, m0, m1);
-      if (r) {
-	fprintf(stderr, "%s: %s: not a valid template file, crib not found\n", NAME, infile);
-	exit(1);
-      }
-      fclose(fout);
-    }
-    fclose(fin);
-  }
+  fclose(fwolf);
+  fclose(fsheep);
+  fclose(fin);
   return 0;
 }
 
--- shepherd.bat
+++ shepherd.bat
@@ -3,18 +3,12 @@
 Release\shepherd.exe %1 %2 > sheep\sc.h
 msbuild /p:Configuration=Release /t:Build sheep\sheep.vcxproj
 
-del long_egg\demo
-del long_egg\a
-del long_egg\b
-del long_egg\wolf.exe
-del long_egg\sheep.exe
+del evilize\a
+del evilize\b
+del evilize\wolf.exe
+del evilize\sheep.exe
 
-copy sheep\Release\sheep.exe long_egg\demo
+cd evilize 
 
-cd long_egg
-for /f "tokens=1,2,3,4,5,6,7,8" %%g in ('php long_egg.php') do IF %%g == Chainring SET out=%%n
-..\fastcoll\fastcoll.exe -i %out% -o a b
-php long_egg.php
+evilize.exe ..\sheep\Release\sheep.exe 
 
-move devil wolf.exe
-move angel sheep.exe
